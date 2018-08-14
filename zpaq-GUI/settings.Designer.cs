5029f521f4bd324f396802857cb12b5e04a2176c
Jackz <me@jackz.me>
2018-08-13 21:12:48 -0500

    redesign extractgui, fixed some bugs

--- zpaq-GUI/settings.Designer.cs
+++ zpaq-GUI/settings.Designer.cs
@@ -34,9 +34,9 @@
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.check_fileassoc = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
@@ -69,7 +69,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(13, 90);
+            this.linkLabel1.Location = new System.Drawing.Point(16, 90);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(81, 13);
             this.linkLabel1.TabIndex = 3;
@@ -80,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 90);
+            this.label2.Location = new System.Drawing.Point(103, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(209, 13);
             this.label2.TabIndex = 4;
@@ -92,20 +92,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Save";
+            this.button1.Text = "Close";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // check_fileassoc
-            // 
-            this.check_fileassoc.AutoSize = true;
-            this.check_fileassoc.Location = new System.Drawing.Point(16, 56);
-            this.check_fileassoc.Name = "check_fileassoc";
-            this.check_fileassoc.Size = new System.Drawing.Size(162, 17);
-            this.check_fileassoc.TabIndex = 6;
-            this.check_fileassoc.Text = "Associate .zpaq with this exe";
-            this.check_fileassoc.UseVisualStyleBackColor = true;
-            this.check_fileassoc.CheckedChanged += new System.EventHandler(this.check_fileassoc_CheckedChanged);
             // 
             // label3
             // 
@@ -129,14 +118,25 @@
             this.label4.TabIndex = 8;
             this.label4.Text = "Developed by Jackz and Caeden";
             // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(16, 52);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(143, 13);
+            this.linkLabel2.TabIndex = 9;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Associate .zpaq with this exe";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
             // settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(567, 157);
+            this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.check_fileassoc);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.linkLabel1);
@@ -165,8 +165,8 @@
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox check_fileassoc;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel2;
     }
 }
